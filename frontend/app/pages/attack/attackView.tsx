41707a64a5a3309f96cc336dab69f1626a3e419d
Errant <48056361+errantSquam@users.noreply.github.com>
2025-11-12 20:22:16 +0800

    different layouts depending on whether attack image is enlarged

--- frontend/app/pages/attack/attackView.tsx
+++ frontend/app/pages/attack/attackView.tsx
@@ -20,33 +20,36 @@ export default function AttackPage() {
     const [enlarge, setEnlarge] = useState(false)
 
     return <div className="flex flex-col items-center pb-10">
-        <div className="flex flex-col gap-y-2 items-center w-2/3">
-            <h1> {attackData?.title}</h1>
-            <div className="italic text-zinc-400">{new Date(attackData?.creationDate || 0).toString()}</div>
+        <div className={`flex ${enlarge ? "flex-col" : "flex-row"} gap-y-2 ${enlarge ? "items-center" : "items-start"} w-2/3`}>
+            <div className="flex flex-col gap-y-2 items-center">
+                <h1> {attackData?.title}</h1>
+                <div className="italic text-zinc-400">{new Date(attackData?.creationDate || 0).toString()}</div>
 
-            <img src={attackData?.image} className={`${enlarge ? "w-full" : "w-2/3"} cursor-pointer`}
-                onClick={() => { setEnlarge(!enlarge) }}
-            />
-            <div className="flex flex-col items-start w-full gap-y-2">
-                <Link to={getProfileLink(userData?.username || '', userData?.uid)} className="flex flex-row gap-x-2">
-                    <img src={getPfp(userData?.profilePicture)} className="w-15" />
-                    <div className="flex flex-col gap-x-1">
-                        <div className="font-bold">{userData?.username}</div>
-                        {userData?.pronouns && <div className="text-zinc-400 italic">({userData?.pronouns})</div>}
+                <img src={attackData?.image} className={`${enlarge ? "w-full" : "w-2/3"} cursor-pointer`}
+                    onClick={() => { setEnlarge(!enlarge) }}
+                />
+                <div className = "italic text-zinc-400">(Click to enlarge; click again to shrink)</div>
+                <div className="flex flex-col items-start w-full gap-y-2 mt-2">
+                    <Link to={getProfileLink(userData?.username || '', userData?.uid)} className="flex flex-row gap-x-2">
+                        <img src={getPfp(userData?.profilePicture)} className="w-15" />
+                        <div className="flex flex-col gap-x-1">
+                            <div className="font-bold">{userData?.username}</div>
+                            {userData?.pronouns && <div className="text-zinc-400 italic">({userData?.pronouns})</div>}
+                        </div>
+                    </Link>
+                    <div className="ml-5 p-2 border-zinc-700 border-2 w-full rounded">
+                        <SanitizedMarkdown markdown={attackData?.description || '*This attack has no description.*'} />
                     </div>
-                </Link>
-                <div className="ml-5 p-2 border-zinc-700 border-2 w-full rounded">
-                    <SanitizedMarkdown markdown={attackData?.description || '*This attack has no description.*'} />
                 </div>
             </div>
-
-            <hr />
-            <div className="flex flex-col items-start w-full gap-y-2">
+            <div className={`flex flex-col items-start justify-start ${enlarge ? "w-full h-auto mt-0" 
+            : "w-1/2 h-full mt-10"} 
+            gap-y-2`}>
                 <h3> Comments </h3>
                 <div className="ml-10 flex flex-col gap-y-2 w-full">
                     {/*placeholder for now */}
                     <div className="w-full mb-10">
-                        <form className = "flex flex-col gap-y-2">
+                        <form className="flex flex-col gap-y-2">
                             <MarkdownEditor ref={commentsRef} />
                             <input type="submit" value="Submit"
                                 className="w-full bg-zinc-700 hover:bg-zinc-600 p-2 cursor-pointer rounded" />
