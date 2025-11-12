eade88c9aab57b12ca3deab24640b81226ef7959
Errant <48056361+errantSquam@users.noreply.github.com>
2025-11-12 19:32:34 +0800

    added spoilers on images

--- frontend/app/components/loaders.tsx
+++ frontend/app/components/loaders.tsx
@@ -1,23 +1,32 @@
 import { Icon } from "@iconify/react"
 import { useState } from "react"
 
-export const ImageSkeletonComponent = ({className} : {className:string}) => {
+export const ImageSkeletonComponent = ({ className }: { className: string }) => {
     return <div className={`flex items-center justify-center ${className}
     rounded-sm bg-gray-700`}>
-        <Icon icon = "material-symbols:image-outline-rounded" className="w-10 h-10 text-gray-200 dark:text-gray-600" />
+        <Icon icon="material-symbols:image-outline-rounded" className="w-10 h-10 text-gray-200 dark:text-gray-600" />
     </div>
 }
 
-export const ImageWithLoader = ({src, className}: {src: string, className:string}) => {
+export const ImageWithLoader = ({ src, className, spoiler = undefined }:
+    { src: string, className: string, spoiler?: string | undefined }) => {
     const [isLoading, setIsLoading] = useState(true)
 
-    return <>
-    <img src = {src} onLoad = {() => setIsLoading(false)} 
-    className = {`${className} ${isLoading ? "opacity-0 absolute":"opacity-100 relative"}`}/>
-    <ImageSkeletonComponent className = {`${className} ${isLoading ? "visible":"hidden"}`}/>
+    return <div className="relative overflow-hidden group">
+        <div className={`absolute opacity-0 group-hover:opacity-100 transition 
+        bottom-0 left-0 right-0 top-0 grid place-items-center text-white z-99
+        text-ellipsis p-6`}>
+            {spoiler}
+            </div>
+        {spoiler !== undefined && 
+        <div className="absolute opacity-0 group-hover:opacity-50 transition bg-zinc-900 z-98 w-full h-full" />}
+        <img src={src} onLoad={() => setIsLoading(false)}
+            className={`${className} ${isLoading ? "opacity-0 absolute" : "opacity-100 relative"}
+        ${spoiler !== undefined ? "blur-md" : "blur-none"}`} />
+        <ImageSkeletonComponent className={`${className} ${isLoading ? "visible" : "hidden"}`} />
 
-    
-    </>
+
+    </div>
 
 
 }
