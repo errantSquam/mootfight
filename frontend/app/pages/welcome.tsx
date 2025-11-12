a24de2c27faf2ccc78ff481063ae9fd51f8107ae
Errant <48056361+errantSquam@users.noreply.github.com>
2025-11-12 13:31:59 +0800

    homepage adjust

--- frontend/app/pages/welcome.tsx
+++ frontend/app/pages/welcome.tsx
@@ -15,9 +15,9 @@ export function Welcome() {
       return []
     }
     let tempArray: DocumentData[] = []
-      snapshotData.forEach((user: QueryDocumentSnapshot<DocumentData, DocumentData>) => {
-        tempArray = [...tempArray, user.data()]
-      })
+    snapshotData.forEach((user: QueryDocumentSnapshot<DocumentData, DocumentData>) => {
+      tempArray = [...tempArray, user.data()]
+    })
     return tempArray
 
   }
@@ -25,21 +25,19 @@ export function Welcome() {
   return (
     <div className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <div className="flex flex-col items-center gap-9 text-xl">
-          Welcome to Mootfight!
-        </div>
         <div className="w-2/3 space-y-6 px-4">
+          <h3 className = "ml-4">Active Users</h3>
+
           <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <div>
-              <b>Active Users</b>
-              <div className = "flex flex-row gap-x-2">
-              {!loading && transformSnapshot(snapshot).map((user) => {
-                return <Link to = {getProfileLink(user.username, user.uid)}>
-                  <div className = "flex flex-col items-center" key = {user.username}>
-                  <ImageWithLoader src = {getPfp(user.profilePicture)} className = "w-20 h-20 object-cover"/>
-                  <span>{user.username}</span>
-                  </div></Link>
-              })}
+              <div className="flex flex-row gap-x-2">
+                {!loading && transformSnapshot(snapshot).map((user) => {
+                  return <Link to={getProfileLink(user.username, user.uid)}>
+                    <div className="flex flex-col items-center" key={user.username}>
+                      <ImageWithLoader src={getPfp(user.profilePicture)} className="w-20 h-20 object-cover" />
+                      <span>{user.username}</span>
+                    </div></Link>
+                })}
               </div>
             </div>
           </div>
